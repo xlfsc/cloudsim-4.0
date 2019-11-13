08be9be1c2487a1f44468dce744bd897b96fcd19
xuliang <xuliang@guanghe.tv>
2019-11-13 16:47:58 +0800

    Redis添加参数

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/container/core/Redis.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/container/core/Redis.java
@@ -1,5 +1,9 @@
 package org.cloudbus.cloudsim.container.core;
 
+import org.cloudbus.cloudsim.container.core.redis.Configuration;
+import org.cloudbus.cloudsim.container.core.redis.ERedisModel;
+import org.cloudbus.cloudsim.container.core.redis.ERedisOperationType;
+import org.cloudbus.cloudsim.container.core.redis.QpsCaculate;
 import org.cloudbus.cloudsim.container.schedulers.ContainerCloudletScheduler;
 
 import java.util.List;
@@ -9,6 +13,15 @@ public class Redis extends Container {
     private int responseTime;
 
     private int qps;
+
+    private int max_connection;
+    private int max_inbound_bandwidth;
+    private int max_outbound_bandwidth;
+    private int max_qps;
+
+    private ERedisModel model;
+
+    private int current_connection = 0;
 
     /**
      * Creates a new Container-based Redis object.
@@ -35,6 +48,17 @@ public class Redis extends Container {
          */
         setResponseTime(100);
         setQps(100);
+
+        init();
+    }
+
+    private void init() {
+        this.max_connection = Configuration.getProperty("redis.MAX_ACTIVE", 1024);
+        this.max_inbound_bandwidth = Configuration.getProperty("redis.max.inbound.bandwidth", 1000);
+        this.max_outbound_bandwidth = Configuration.getProperty("redis.max.outbound.bandwidth", 1000);
+        this.max_qps = Configuration.getProperty("redis.max.qps", 1000);
+
+        this.model = ERedisModel.valueOf(Configuration.getProperty("redis.model", "STANDARDDOUBLE"));
     }
 
     public int getResponseTime() {
@@ -47,6 +71,19 @@ public class Redis extends Container {
 
     public int getQps() {
         return qps;
+    }
+
+    private int qps() {
+        return QpsCaculate.QpsCaculate(getId(), 0, current_connection, max_connection, getBw()
+                , getMips(), model.getStatus(), max_inbound_bandwidth, max_outbound_bandwidth);
+    }
+
+    private int qps(ERedisOperationType type) {
+        int qps = qps();
+        switch (type) {
+            default:
+                return qps;
+        }
     }
 
     public void setQps(int qps) {
